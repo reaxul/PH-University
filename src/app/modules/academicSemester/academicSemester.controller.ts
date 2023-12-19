9656351fe3219a733c0ea956e1e076d9b2a4f84d
DevReaxul <hreazul11@gmail.com>
2023-12-19 21:43:17 +0600

    academic semester code setup complete

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,18 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { AcademicSemesterServices } from "./academicSemester.service";
+
+const createAcademicSemester = catchAsync(async (req, res) => {
+    const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(req.body)
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Semester successfully created',
+        data:result,
+    })
+})
+
+export const AcademicSemesterController = {
+    createAcademicSemester,
+}
