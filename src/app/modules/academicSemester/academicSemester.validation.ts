b8df07be4d1cff73707cefbccbb66f98002e3fc5
DevReaxul <hreazul11@gmail.com>
2023-12-21 01:35:00 +0600

    semester creating api code updated

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -7,7 +7,7 @@ const createAcademicSemesterValidationSchema = z.object({
         year: z.string(),
         code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
         startMonth: z.enum([...Months] as [string, ...string[]]),
-        endMonth: z.enum([...Months] as [string, ...string[]])
+        endMonth: z.enum([...Months] as [string, ...string[]]),
     }),
 })
 
@@ -15,9 +15,9 @@ const updateAcademicSemesterValidationSchema = z.object({
     body: z.object({
         name: z.enum([...AcademicSemesterName] as [string, ...string[]]).optional(),
         year: z.string().optional(),
-        code:z.enum([...AcademicSemesterCode]as [string, ...string[]]).optional(),
-        startMonth:z.enum([...Months]as [string, ...string[]]).optional(),
-        endMonth:z.enum([...Months]as [string, ...string[]]).optional(),
+        code: z.enum([...AcademicSemesterCode] as [string, ...string[]]).optional(),
+        startMonth: z.enum([...Months] as [string, ...string[]]).optional(),
+        endMonth: z.enum([...Months] as [string, ...string[]]).optional(),
     })
 })
 
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,11 +1,12 @@
 import config from '../../config';
-import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
+import { generatedStudentId } from './user.utils';
 
-const createStudentIntoDB = async (password: string, studentData: TStudent) => {
+const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // create a user object
   const userData: Partial<TUser> = {};
 
@@ -15,12 +16,12 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
   //set student role
   userData.role = 'student';
 
-  const generatedStudentId = (payload: TAcademicSemester) => {
-    
-  }
+
+
+  const admissionSemester = await AcademicSemester.findById(payload.admissionSemester)
 
   //set manually generated it
-  userData.id = generatedStudentId();
+  userData.id = await generatedStudentId(admissionSemester);
 
   // create a user
   const newUser = await User.create(userData);
@@ -28,10 +29,10 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
   //create a student
   if (Object.keys(newUser).length) {
     // set id , _id as user
-    studentData.id = newUser.id;
-    studentData.user = newUser._id; //reference _id
+    payload.id = newUser.id;
+    payload.user = newUser._id; //reference _id
 
-    const newStudent = await Student.create(studentData);
+    const newStudent = await Student.create(payload);
     return newStudent;
   }
 };
